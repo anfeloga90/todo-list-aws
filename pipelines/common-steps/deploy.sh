ce0a9b5022726d1541be2f9234d2e6cef097f42e
@NDRESf9002 <anfeloga90>
2022-05-20 01:33:59 +0000

    update deploy.sh remove some lines

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -3,5 +3,4 @@
 set -x
 du -hs * | sort -h
 echo ${ENVIRONMENT} 
-echo sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
-sam deploy template.yaml --config-env staging --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
